48af55a5ee50eebea347883099f74e3552ed457f
Alexei A. Smekalkine <ikle@ikle.ru>
2020-10-27 05:36:26 +0300

    Implement IGB methods

---
+++ ops-igb.c
@@ -0,0 +1,85 @@
+/*
+ * IGB methods
+ *
+ * Copyright (c) 2020 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <errno.h>
+
+#include "ethtool.h"
+
+static int link_calc_sum (struct link *o, int iv, unsigned offset, int count)
+{
+	unsigned s, i, w;
+
+	for (s = iv, i = offset; count > 0; ++i, --count) {
+		if (!link_read_word (o, i, &w))
+			return -1;
+
+		s += w;
+	}
+
+	return s;
+}
+
+#define IGB_EEPROM_CHECKSUM	0x3f
+#define IGB_EEPROM_SUM		0xbaba
+
+static int igb_calc_checksum (struct link *o, unsigned *sum)
+{
+	int s;
+
+	if ((s = link_calc_sum (o, 0, 0, IGB_EEPROM_CHECKSUM)) < 0)
+		return 0;
+
+	*sum = (IGB_EEPROM_SUM - s) & 0xffff;
+	return 1;
+}
+
+static int igb_validate_checksum (struct link *o)
+{
+	unsigned sum, real;
+
+	if (!link_read_word (o, IGB_EEPROM_CHECKSUM, &sum) ||
+	    !igb_calc_checksum (o, &real))
+		return 0;
+
+	if (sum != real) {
+		errno = EBADMSG;
+		return 0;
+	}
+
+	return 1;
+}
+
+static int igb_update_checksum (struct link *o)
+{
+	unsigned sum;
+
+	return	igb_calc_checksum (o, &sum) &&
+		link_write_word (o, IGB_EEPROM_CHECKSUM, sum);
+}
+
+static int igb_read_id (struct link *o, unsigned *vendor, unsigned *device)
+{
+	return	igb_validate_checksum (o)        &&
+		link_read_word (o, 0x0b, device) &&
+		link_read_word (o, 0x0c, vendor);
+}
+
+static int igb_write_id (struct link *o, unsigned vendor, unsigned device)
+{
+	return	igb_validate_checksum (o)         &&
+		link_write_word (o, 0x0b, device) &&
+		link_write_word (o, 0x0c, vendor) &&
+		igb_update_checksum (o);
+}
+
+struct link_ops igb_ops = {
+	.validate_checksum	= igb_validate_checksum,
+	.update_checksum	= igb_update_checksum,
+	.read_id		= igb_read_id,
+	.write_id		= igb_write_id,
+};
--- table.c
+++ table.c
@@ -10,7 +10,57 @@
 
 /* Device-specific operations table */
 
+extern struct link_ops igb_ops;
+
 struct table table[] = {
+	/* e1000e */
+
+	{ 0x10d38086, &igb_ops		},	/* 82574L		*/
+	{ 0x15028086, &igb_ops		},	/* 82579LM		*/
+
+	/* igb */
+
+	{ 0x04388086, &igb_ops		},	/* DH89xxCC, sgmii	*/
+	{ 0x043a8086, &igb_ops		},	/* DH89xxCC, serdes	*/
+	{ 0x043c8086, &igb_ops		},	/* DH89xxCC, backplane	*/
+	{ 0x04408086, &igb_ops		},	/* DH89xxCC, sfp	*/
+
+	{ 0x10a78086, &igb_ops		},	/* 82575EB, copper	*/
+	{ 0x10a98086, &igb_ops		},	/* 82575EB, fiber	*/
+	{ 0x10c98086, &igb_ops		},	/* 82576, copper	*/
+	{ 0x10d68086, &igb_ops		},	/* 82575GB, quad copper	*/
+	{ 0x10e68086, &igb_ops		},	/* 82576, fiber		*/
+	{ 0x10e78086, &igb_ops		},	/* 82576, serdes	*/
+	{ 0x10e88086, &igb_ops		},	/* 82576, quad copper	*/
+
+	{ 0x150a8086, &igb_ops		},	/* 82576NS, copper	*/
+	{ 0x150d8086, &igb_ops		},	/* 82576, quad serdes	*/
+	{ 0x150e8086, &igb_ops		},	/* 82580, copper	*/
+	{ 0x150f8086, &igb_ops		},	/* 82580, fiber		*/
+	{ 0x15108086, &igb_ops		},	/* 82580, serdes	*/
+	{ 0x15118086, &igb_ops		},	/* 82580, sgmii		*/
+	{ 0x15168086, &igb_ops		},	/* 82580, dual copper	*/
+	{ 0x15188086, &igb_ops		},	/* 82576NS, serdes	*/
+	{ 0x15218086, &igb_ops		},	/* I350, copper		*/
+	{ 0x15228086, &igb_ops		},	/* I350, fiber		*/
+	{ 0x15238086, &igb_ops		},	/* I350, serdes		*/
+	{ 0x15248086, &igb_ops		},	/* I350, sgmii		*/
+	{ 0x15268086, &igb_ops		},	/* 82576, quad copper ET2 */
+	{ 0x15278086, &igb_ops		},	/* 82580, quad fiber	*/
+
+	{ 0x15338086, &igb_ops		},	/* I210, copper		*/
+	{ 0x15368086, &igb_ops		},	/* I210, fiber		*/
+	{ 0x15378086, &igb_ops		},	/* I210, serdes		*/
+	{ 0x15388086, &igb_ops		},	/* I210, sgmii		*/
+	{ 0x15398086, &igb_ops		},	/* I211, copper		*/
+
+	{ 0x157b8086, &igb_ops		},	/* I210, copper	flashless */
+	{ 0x157c8086, &igb_ops		},	/* I210, serdes flashless */
+
+	{ 0x1f408086, &igb_ops		},	/* I354, backplane 1.0	*/
+	{ 0x1f418086, &igb_ops		},	/* I354, sgmii		*/
+	{ 0x1f458086, &igb_ops		},	/* I354, backplane 2.5	*/
+
 	/* terminator */
 
 	{				}
